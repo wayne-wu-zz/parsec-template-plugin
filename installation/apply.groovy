cc86e4376bc49f31b83fe7603c9add52979c254a
Wayne Wu <waynewu@yahoo-inc.com>
2016-06-25 01:38:21 +0800

    update

--- installation/apply.groovy
+++ installation/apply.groovy
@@ -10,7 +10,7 @@ buildscript{
     dependencies{
         classpath group: 'gradle.plugin.com.yahoo.parsec',
                 name: 'parsec-template-plugin',
-                version: '1.0.7-pre'
+                version: '1.0.8-pre'
     }
 }
 
@@ -18,11 +18,3 @@ allprojects {
     apply plugin: com.yahoo.parsec.parsec_templates.ParsecTemplatesPlugin
 }
 
-
-
-//TODO: see why the code below does not work
-/*
-if(!project.plugins.findPlugin(com.yahoo.parsec.parsec_templates.ParsecTemplatesPlugin)){
-    project.apply(plugin: com.yahoo.parsec.parsec_templates.ParsecTemplatesPlugin)
-}
-*/
