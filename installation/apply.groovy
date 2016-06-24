ee1d65b9782f40cdb6875a0adbae67e2b0f889bb
Wayne Wu <waynewu@yahoo-inc.com>
2016-06-25 01:03:58 +0800

    latest version

--- installation/apply.groovy
+++ installation/apply.groovy
@@ -10,7 +10,7 @@ buildscript{
     dependencies{
         classpath group: 'gradle.plugin.com.yahoo.parsec.parsec-templates',
                 name: 'parsec-templates',
-                version: '1.1.6'
+                version: '1.0.7-pre'
     }
 }
 
